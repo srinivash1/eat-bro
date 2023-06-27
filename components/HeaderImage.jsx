1b42772f5ad65bbab9f9e16590e7341da3c04b0e
srinivash1 <135028964+srinivash1@users.noreply.github.com>
2023-06-27 16:58:21 +0530

    Create HeaderImage.jsx

---
+++ components/HeaderImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderImage = () => {
+  return (
+    <div className='container'>
+        <img className='header-image' src='https://eatcleanbro.com/media/wysiwyg/banner/eatcleanbro2023.jpg'/>
+        <div class="centered">Hey Bro Eat this shit</div>
+    </div>
+  )
+}
+
+export default HeaderImage
